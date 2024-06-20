eada4024fc0fb618c58ffd4ccad68ed7dbf3b65f
Mythic1Sandwich <Whatsyourdamage11@gmail.com>
2024-06-20 15:02:53 +0300

    new

--- abdulaeva2213210/gameboard.h
+++ abdulaeva2213210/gameboard.h
@@ -10,23 +10,24 @@
 #include <QMessageBox>
 #include <vector>
 #include <string>
-
 class GameBoard : public QWidget {
     Q_OBJECT
 
 public:
     GameBoard(QWidget *parent = nullptr);
     ~GameBoard();
+    void generatePromoCodes(int count);
     void addNewPromoCard();
-
 private slots:
     void onRevealButtonClicked();
 
+
 private:
     QGridLayout *gridLayout;
-   // std::vector<QLineEdit*> promoCards;
+    std::vector<QLineEdit*> promoCards;
     std::vector<std::string> promoCodes;
     QPushButton *revealButton;
 };
+
 
 #endif // GAMEBOARD_H
